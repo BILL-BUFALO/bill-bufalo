9383fc9d9e0f2009ad9486c5bd49de80c667ecc1
BILL-BUFALO <60087592+BILL-BUFALO@users.noreply.github.com>
2020-01-24 10:36:09 +0800

    Update mailin.sh

--- .config/polybar/scripts/mailin.sh
+++ .config/polybar/scripts/mailin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mails=$(curl -u gb1.irk@gmail.com:wwlxsknetzxxmrot --silent "https://mail.google.com/mail/feed/atom" | tr -d '\n' | awk -F '<entry>' '{for (i=2; i<=NF; i++) {print $i}}' | wc -l)
+mails=$(curl -u gb1.irk@gmail.com:wwlxsknetzxxmrotz --silent "https://mail.google.com/mail/feed/atom" | tr -d '\n' | awk -F '<entry>' '{for (i=2; i<=NF; i++) {print $i}}' | wc -l)
 
 if [ "$mails" -eq "0" ]; then
 echo " 0"
