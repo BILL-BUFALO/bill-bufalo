bc51eecb881b51406aa4cead83b99591dfc85327
BILL-BUFALO <60087592+BILL-BUFALO@users.noreply.github.com>
2020-01-24 09:31:25 +0800

    Create mailin.sh

---
+++ .config/polybar/scripts/mailin.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mails=$(curl -u gb1.irk@gmail.com:wwlxsknetzxxmrot --silent "https://mail.google.com/mail/feed/atom" | tr -d '\n' | awk -F '<entry>' '{for (i=2; i<=NF; i++) {print $i}}' | wc -l)
+
+if [ "$mails" -eq "0" ]; then
+echo " 0"
+else
+echo " $mails"
+fi
