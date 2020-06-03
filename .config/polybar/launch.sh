da8f452252c452ce72acc7e88d8599f9791c1a3c
BILL-BUFALO <60087592+BILL-BUFALO@users.noreply.github.com>
2020-06-03 09:58:44 +0800

    Add files via upload

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Завершить текущие экземпляры polybar
+killall -q polybar
+
+# Ожидание полного завершения работы процессов
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar noteboock -r &
+polybar example -r &
+# polybar noteboock -r &
